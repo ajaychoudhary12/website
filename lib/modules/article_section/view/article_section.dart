42d221a92067afcf43c956c09e0f538f48034f6f
Ajay Choudhary <ajaychoudhary19990@gmail.com>
2024-02-06 23:05:05 +0530

    Made ListView width responsive

--- lib/modules/article_section/view/article_section.dart
+++ lib/modules/article_section/view/article_section.dart
@@ -7,11 +7,24 @@ class ArticleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: articles.length,
-      itemBuilder: (context, index) {
-        return ArticleItemWidget(article: articles[index]);
-      },
+    double fixedWidth = 850.0;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Align(
+      alignment: Alignment.topCenter,
+      child: SizedBox(
+        width: screenWidth > fixedWidth ? fixedWidth : screenWidth,
+        child: SizedBox(
+          width: fixedWidth,
+          child: ListView.builder(
+            itemCount: articles.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return ArticleItemWidget(article: articles[index]);
+            },
+          ),
+        ),
+      ),
     );
   }
 }
